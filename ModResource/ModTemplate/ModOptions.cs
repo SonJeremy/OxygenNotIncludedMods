6f328a400dc1f17bbd44e379f3ec444bb806b04f
Lê Minh Sơn <leminhson2142@gmail.com>
2025-03-20 16:34:24 +0700

    Add Template for Easily Create or Fix a Mod

---
+++ ModResource/ModTemplate/ModOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+//#if UsePLib
+
+using PeterHan.PLib;
+using PeterHan.PLib.Options;
+//#endif
+
+namespace SonJeremy.ModTemplate
+{
+    //#if (UsePLib)
+    [Serializable]
+    [RestartRequired]
+    [ConfigFile(IndentOutput: true, SharedConfigLocation: true)]
+    public sealed class ModOptions : SingletonOptions<ModOptions>
+    {
+        
+    }
+    //#else
+    public sealed class ModOptions
+    {
+        
+    }
+    //#endif
+}
